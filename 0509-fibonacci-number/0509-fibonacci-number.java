c5fc871f1672b21271c0b80a209a779a6806cc34
Anoop Kushwaha <186592607+Anoopk51@users.noreply.github.com>
2026-07-23 12:32:50 +0530

    Time: 11 ms (9.34%), Space: 42 MB (70.16%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.java
+++ 0509-fibonacci-number/0509-fibonacci-number.java
@@ -6,8 +6,6 @@ class Solution {
         if (n == 1){
             return 1;
         }
-        int fibo = fib(n-1) + fib(n - 2);
-        return fibo;
-
-    }
+         return  fib(n-1) + fib(n - 2);
+       }
 }
